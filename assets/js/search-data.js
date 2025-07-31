58f29011e7c4d3cd95b7bcac1972f54ae27de06a
LucaDiPierno <168741231+LucaDiPierno@users.noreply.github.com>
2025-07-31 14:05:13 +0000

    Deploying to gh-pages from @ LucaDiPierno/LucaDiPierno.github.io@27beccbc9141764bf5c2396ccf9f5c78b48bbf1d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -457,7 +457,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6C%75%63%61.%64%69%70%69%65%72%6E%6F@%61%61%75.%61%74", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/LucaDiPierno", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -465,6 +472,13 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/lucadipierno", "_blank");
         },
       },{
         id: 'social-rss',
@@ -478,14 +492,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=BQMRirUAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
