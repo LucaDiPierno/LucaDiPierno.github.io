66476b0a97ca6c5d18c18a3fefc6d5e37778a8ba
LucaDiPierno <168741231+LucaDiPierno@users.noreply.github.com>
2025-07-31 12:55:14 +0000

    Deploying to gh-pages from @ LucaDiPierno/LucaDiPierno.github.io@bfc0bf9f4e56726341d176fb2e19c8e156d82fae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "An overview of projects I have been involved in.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
